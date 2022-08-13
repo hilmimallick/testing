fe1dff79fdd1f244bd046a15988efe0a7c3751c1
unknown <hilmi09mallick@gmail.com>
2022-08-13 21:02:52 +0200

    testing

---
+++ public/product.js
@@ -0,0 +1,28 @@
+let products = [];
+const productContainer = document.getElementById("products-data");
+fetch("https://node-pair.herokuapp.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    products = data;
+    console.log(data);
+    showproducts(data);
+  });
+function showproducts(products) {
+  productContainer.innerHTML = "";
+  products.forEach((product) => {
+    productContainer.innerHTML += `
+    <div class="col-md-3">
+      <div class="card" style="width: 18rem;">
+  <img src="${product.imgURL}" class="card-img-top">
+  <div class="card-body">
+    <p class="card-text">${product.title}</p>
+    <p class="card-text">${product.category}</p>
+    <p class="card-text">R${product.price}</p>
+    <p class="card-text">${product.description}</p>
+    <p class="card-text">${product.gender}</p>
+  </div>
+</div>
+</div>
+    `;
+  });
+}
