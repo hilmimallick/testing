f5016b7cbe9a08cd647fcabbe46cb8781ae3a11d
unknown <hilmi09mallick@gmail.com>
2022-08-13 20:55:18 +0200

    testing

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const con = require("../lib/db_Connection");
+const con = require("../lib/db_connection");
 
 
 router.get("/", (req, res) => {
