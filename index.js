da8bfff32c1969a0eb5994717433b7bff8b326e4
unknown <hilmi09mallick@gmail.com>
2022-08-13 19:03:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express"); // Used to set up a server
+const cors = require("cors"); // Used to prevent errors when working locally
+
+const app = express(); // Initialize express as an app variable
+app.set("port", process.env.PORT || 6000); // Set the port
+app.use(express.json()); // Enable the server to handle JSON requests
+app.use(cors()); // Dont let local development give errors
+
+app.get("/", (req, res) => {
+    res.json({ msg: "Welcome" });
+});
+
+app.listen(app.get("port"), () => {
+    console.log(`Listening for calls on port ${app.get("port")}`);
+    console.log("Press Ctrl+C to exit server");
+});
---
+++ routes/productsRoute.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const con = require("../lib/dbConnection");
+
+router.get("/", (req, res) => {
+    try {
+        con.query("SELECT * FROM products", (err, result) => {
+            if (err) throw err;
+            res.send(result);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error)
+    }
+});
+
+module.exports = router;
