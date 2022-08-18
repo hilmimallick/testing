b2347f0b3a59d67e365bc8314175c79239ed5485
hilmimallick <hilmi09mallick@gmail.com>
2022-08-18 12:45:16 +0200

    fixed

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -44,18 +44,31 @@ router.post("/", (req, res) => {
   }
 });
 
-// router.put("/:id", (req, res) => {
-//     const{
-//          sku, name, price, weight, description, thumbnail, image, category, create_date, stock}= req.body
-//     try {
-//         con.query(`UPDATE  FROM products SET sku='${sku}', name='${name}', price='${price}', weight='${weight}'description='${description}', thumbnail='${thumbnail}' image ='${image}', category='${category}', create_date='${create_date}', stock='${stock}',   WHERE products =${req.params.id}`, (err, result) => {
-//             if (err) throw err;
-//             res.send(result);
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         res.status(400).send(error)
-//     }
-// });
+router.put("/:id", (req, res) => {
+  const {
+    sku,
+    name,
+    price,
+    weight,
+    description,
+    thumbnail,
+    image,
+    category,
+    create_date,
+    stock,
+  } = req.body;
+  try {
+    con.query(
+      `UPDATE  FROM products SET sku='${sku}', name='${name}', price='${price}', weight='${weight}'description='${description}', thumbnail='${thumbnail}' image ='${image}', category='${category}', create_date='${create_date}', stock='${stock}',   WHERE products =${req.params.id}`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
 
 module.exports = router;
